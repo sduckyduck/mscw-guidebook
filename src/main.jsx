6e7cde086c3b2054a2871d08974f241e8bc2a42d
sduckyduck <hqywcyhqf@gmail.com>
2026-05-05 21:25:12 -0400

    Use compact hero controls app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AppDashboard from './AppDashboard.jsx';
+import AppCompact from './AppCompact.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppDashboard />
+    <AppCompact />
   </React.StrictMode>,
 );
