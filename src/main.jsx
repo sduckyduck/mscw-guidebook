6a890760055ffa516ee56d0eb40e2858c6d36ba7
sduckyduck <hqywcyhqf@gmail.com>
2026-05-05 21:20:29 -0400

    Use dashboard app entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import AppDashboard from './AppDashboard.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppDashboard />
   </React.StrictMode>,
 );
