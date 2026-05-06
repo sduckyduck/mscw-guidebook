a9e07cf1e537cc7eff841a732252f4fb7d3b7815
sduckyduck <hqywcyhqf@gmail.com>
2026-05-06 11:35:29 -0400

    Show book intro before guidebook app

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppMediaEnhanced from './AppMediaEnhanced.jsx';
+import CoverIntro from './components/CoverIntro.jsx';
 import './styles.css';
+import './styles/cover-intro.css';
+
+function AppWithIntro() {
+  const [entered, setEntered] = useState(false);
+  return entered ? <AppMediaEnhanced /> : <CoverIntro onEnter={() => setEntered(true)} />;
+}
 
 const container = document.querySelector('#root');
 const app = createRoot(container);
 
 app.render(
   <React.StrictMode>
-    <AppMediaEnhanced />
+    <AppWithIntro />
   </React.StrictMode>,
 );
