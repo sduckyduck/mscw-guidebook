b23137c5571b533b52494b061369eeec13158b37
sduckyduck <hqywcyhqf@gmail.com>
2026-05-06 21:13:20 -0400

    Route map recommendations to V2 engine

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_ACTIONS ? '/mscw-guidebook/' : '/',
+  resolve: {
+    alias: {
+      './engine/recommendationEngine.js': '/src/engine/recommendationEngineV2.js',
+    },
+  },
 });
