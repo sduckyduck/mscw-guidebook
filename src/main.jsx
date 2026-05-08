2acdf4187e97c15f8bf9e44aebd6cba9513d0c99
sduckyduck <hqywcyhqf@gmail.com>
2026-05-08 10:56:53 -0400

    Load official data sanity patch

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import CoverIntro from './components/CoverIntro.jsx';
 import CraftingMaterialsPageSafe from './components/CraftingMaterialsPageSafe.jsx';
 import CommunityBuildsPage from './components/CommunityBuildsPage.jsx';
 import AdminBuildsPage from './components/AdminBuildsPage.jsx';
+import './utils/officialDataSanityPatch.js';
 import './utils/dashboardActionButtons.js';
 import './utils/mobileSwipeTabs.js';
 import './utils/removeZakumHelmet.js';
